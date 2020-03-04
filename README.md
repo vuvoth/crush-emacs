1f6bda07a2953ebda476d920378258547e9883cd
Vu Vo Thanh <vu.voth@gmail.com>
2020-03-04 22:53:32 +0700

    update REAME.md

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
-# Emacs Crush 
+# lonely 
 
-Config when i sad. 
-
-
-
-
-
-
-
-
+I'm sadding... 
 Maybe she don't like me :'(.
+
+# I give up everything or I will have nothing
+
+   You, you, you’re the one who comes and breaks the rule  
+   breaks the rule  
+   I, I, I have always been acting a fool  
+   and I know it’s not cool  
+   choose ego over u  
+   even I know the truth  
+   I love you  
+
+   Don’t care, don’t hear  
+   what they say, they do  
+   to your life, your story.  
+
+   You might not... be the first, or the last, but the best...  
+
+                                                         Han Han  
 
 
--- init.el
+++ init.el
@@ -20,18 +20,20 @@
 
 (add-to-list 'load-path "~/.emacs.d/config")
 
-(require 'general-settings)
+
 (require 'interface)
-(require 'git-configure)
 (require 'navigative)
 (require 'programming-config)
+(require 'git-configure)
+(require 'utils)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(treemacs-projectile which-key use-package smex restart-emacs prettier-js lsp-ui lsp-treemacs lsp-ivy exec-path-from-shell diff-hl counsel-projectile company-lsp all-the-icons)))
+   '(google-this magit seti-theme zen-and-art-theme dockerfile-mode yaml-mode treemacs-projectile which-key use-package smex restart-emacs prettier-js lsp-ui lsp-treemacs lsp-ivy exec-path-from-shell diff-hl counsel-projectile company-lsp all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
