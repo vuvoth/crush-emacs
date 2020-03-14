7a48638222647f78e71e15c76b46c663761c9e50
Vu Vo Thanh <vu.voth@gmail.com>
2020-03-15 00:58:59 +0700

    initial kei-modeline.el

---
+++ modeline/kei-modeline.el
@@ -0,0 +1,8 @@
+
+;; Kei modeline
+;; Simple mode line for keimacs
+;; vuvoth / vuvth - Vu Vo Thanh 
+;; Hello, hello, hello, how low
+;; Hello, hello, hello, how low
+;; Hello, hello, hello, how low
+;; Hello, hello, hello
